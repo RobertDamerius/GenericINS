c33299b1b954e964b8ca62d8be58f3def14289fe
Robert Damerius <damerius.mail@gmail.com>
2022-03-09 15:58:25 +0100

    Updated the gravity model based on MATLAB's gravitywgs84 implementation.

--- packages/+GenericINS/SensorFusion.m
+++ packages/+GenericINS/SensorFusion.m
@@ -13,6 +13,7 @@
 % 20210728    Robert Damerius        Using a more accurate transformation in the position sensor model.
 % 20220307    Robert Damerius        Initialized internal state with valid quaternion. Updated internal normalization to produce a zero vector with a one in the first component.
 %                                    If the filter is reset, no prediction/updates are performed at the same time. A prediction in the next time-step is required to make the filter valid.
+% 20220309    Robert Damerius        Updated the gravity model based on MATLAB's gravitywgs84 implementation.
 % 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 classdef SensorFusion < handle
@@ -1262,27 +1263,97 @@ classdef SensorFusion < handle
             % Re    ... Eastern earth radius [m].
             % R0    ... Mean earth radius [m].
             a = 6378137.0;
-            ee = 0.0066943799901410724656084644; % e*e, e=0.081819190842621
+            ee = 0.00669437999014132;
             s = sin(latitude);
             invRoot = 1.0 / sqrt(1 - ee * s * s);
             Re = a * invRoot;
             Rn = Re * (1-ee) * (invRoot*invRoot);
             R0 = sqrt(Re * Rn);
         end
-        function g = Gravity(altitude, latitude, R0)
-            %GenericINS.SensorFusion.Gravity Compute the local gravity g [m/s^2] for a given position using the WGS84 earth reference model.
+        function gravity = GravityWGS84(latitude, longitude, altitude)
+            %GenericINS.SensorFusion.GravityWGS84 Calculate the gravity according to the WGS84 reference model. This implementation
+            % is based on MATLAB's gravitywgs84 function using the close approximation method, e.g. gravitywgs84(h,lat,lon,'CloseApprox',[false false false 0])
+            % including atmosphere and centrifugal effects and excluding the presence of a precessing reference frame.
             % 
             % PARAMETERS
-            % altitude ... Height [m] w.r.t. the WGS84 reference ellipsoid (positive upwards).
-            % latitude ... Geographical latitude [rad] for the position of interest.
-            % R0       ... Mean earth radius [m] (WGS84).
+            % latitude   ... [double x 1] Latitude in radians.
+            % longitude  ... [double x 1] Longitude in radians.
+            % altitude   ... [double x 1] Altitude in meters, positive upwards.
             % 
             % RETURN
-            % g ... Scalar local gravity [m/s^2].
-            s = sin(latitude);
-            s2 = sin(2 * latitude);
-            R = R0 / (R0 + altitude);
-            g = 9.780318 * (1 + 5.3024e-3 * s*s + 5.9e-6 * s2*s2) * R * R;
+            % gravity    ... [double x 1] Gravity in m/s^2
+            % 
+            % DETAILS
+            % Use of the WGS84 Close Approximation model should be limited to a geodetic height of 20,000.0 meters (approximately 65,620.0 feet).
+            % Below this height, it gives results with submicrogal precision (see help of gravitywgs84).
+            assert(isscalar(latitude) && isscalar(longitude) && isscalar(altitude), 'Inputs must be scalar!');
+
+            % Some constants
+            E        = 5.2185400842339e+5;  % = WGS->E;
+            E2       = E*E;                 % = WGS84->E * WGS84->E;
+            GM       = 3986004.418e+8;      % = WGS84->GM_default;
+            a        = 6378137.0;           % = WGS->a;
+            b        = 6356752.3142;        % = WGS->b;
+            e2       = 6.69437999014e-3;    % = WGS->e2;
+            b_over_a = 0.996647189335;      % = WGS->b_over_a;
+            omega    = 7.292115e-5;         % = WGS->omega_default;
+
+            % Precalculation of sin/cos
+            sinphi = sin(latitude);
+            cosphi = cos(latitude);
+            sinlambda = sin(longitude);
+            coslambda = cos(longitude);
+            sin2phi = sinphi * sinphi;
+
+            %/* Radius of Curvature in prime vertical (N) /eq. 4-15/ */
+            N = (a) / (sqrt(1.0 - (e2) * sin2phi));
+
+            %/* Calculate rectangular coordinates /eq. 4-14/ */
+            x_rec = (N + altitude) * cosphi * coslambda;
+            y_rec = (N + altitude) * cosphi * sinlambda;
+            z_rec = ((b_over_a) * (b_over_a) * N + altitude) * sinphi;
+
+            %/* Calculate various parameters */
+            D = x_rec * x_rec + y_rec * y_rec + z_rec * z_rec - E2;
+            u2 = 0.5 * D * (1.0 + sqrt(1.0 + 4.0 * E2 * z_rec * z_rec / (D * D)));
+            u2E2 = u2 + E2;
+
+            %/* /eq. 4-8/ */
+            u = sqrt(u2);
+
+            %/* /eq. 4-9/ */
+            beta = atan(z_rec * sqrt(u2E2) / (u * sqrt(x_rec * x_rec + y_rec * y_rec)));
+
+            %/* generate common sines and cosines */
+            sinbeta = sin(beta);
+            cosbeta = cos(beta);
+            sin2beta = sinbeta * sinbeta;
+            cos2beta = cosbeta * cosbeta;
+
+            %/* /eq. 4-10/ */
+            w = sqrt((u2 + E2 * sin2beta) / (u2E2));
+
+            %/* /eq. 4-11/ */
+            q = 0.5 * ((1.0 + 3.0 * u2 / (E2)) * atan((E) / u) - 3.0 * u / (E));
+
+            %/* /eq. 4-12/ */
+            qo = 0.5 * ((1.0 + 3.0 * (b) * (b) / (E2)) * atan((E) / (b)) - 3.0 * (b) / (E));
+
+            %/* /eq. 4-13/ */
+            q_prime = 3.0 * ((1.0 + u2 / (E2)) * (1.0 - (u / (E)) * atan((E) / u))) - 1.0;
+
+            %/* Use Centrifugal Force */
+            cf_u = u * cos2beta * omega * omega / w;
+            cf_beta = sqrt(u2E2) * cosbeta * sinbeta * omega * omega / w;
+
+            %/* /eq. 4-5/ */
+            gamma_u = -(GM / u2E2 + omega * omega * (a) * (a) * (E) * q_prime * (0.5 * sin2beta - 1.0 / 6.0) / (u2E2 * qo)) / w + cf_u;
+
+            %/* /eq. 4-6/ */
+            gamma_beta = omega * omega * (a) * (a) * q * sinbeta * cosbeta / (sqrt(u2E2) * w * qo) - cf_beta;
+
+            % Resulting gravity
+            gravity = sqrt(gamma_u*gamma_u + gamma_beta*gamma_beta);
         end
         function [lat, lon] = LatLon(phi, lambda)
             %GenericINS.SensorFusion.LatLon Convert phi [rad] and lambda [rad] to latitude [rad] and longitude [rad] with latitude being in range [-pi/2, +pi/2] and longitude being in range [-pi; +pi).
@@ -1391,8 +1462,8 @@ classdef SensorFusion < handle
             xOut = x;
 
             % WGS84 properties
-            [Rn, Re, R0] = GenericINS.SensorFusion.WGS84(x(1));
-            localGravity = GenericINS.SensorFusion.Gravity(x(3), x(1), R0);
+            [Rn, Re, ~] = GenericINS.SensorFusion.WGS84(x(1));
+            localGravity = GenericINS.SensorFusion.GravityWGS84(x(1), x(2), x(3));
 
             % Inputs: remove estimated biases and align to b-frame
             f_ib = dcmIMUBody2Sensor' * (u(1:3) + w(1:3) - x(11:13));
